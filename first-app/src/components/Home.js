f9f52f1a1e8bc9fd4e1c5f58f755ded95af6457b
Teena <teena2801choudhary@gmail.com>
2024-09-02 21:32:52 +0530

    final

--- first-app/src/components/Home.js
+++ first-app/src/components/Home.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import Navbar from "./Navbar";
+// import BannerBackground from "../Assets/home-banner-background.png";
+import { FiArrowRight } from "react-icons/fi";
 
 export const Home = () => {
   return (  <div className="home-container">
         <Navbar/>
+        <div className="home-banner-container" >
+          {/* <div className="home-bannerImage-conatainer" >
+               <img src={BannerBackground} alt="" />
+          </div> */}
+          <div className="home-text-section" >
+               <h1 className="primary-heading" >
+                   HEY THERE , TEENA CHOUDHARY HERE !!
+                   </h1>
+                   <p className="primary-text">
+                     I  AM  FROM  ECE-AI  BRANCH  OF  IGDTUW  ,  I  ATTENDED  ASSETMERKEL'S  ORIENTATION 
+                      AND  I  WAS  VERY  INSPIRED  BY  IT  ,  NOW  I  REALLY  WANTS  TO  WORK  WITH  YOU  GUYS  AND
+                      ALSO  WANTS  TO  ENJOY  MORE  PIZZA  SLICES.  
+                                         HAHAHA!!
+                   </p>
+                   <button className="secondary-button" >
+                    Select Me < FiArrowRight />
+                   </button>
+          </div>
+        </div>
     </div>
   );
 
--- first-app/src/components/Navbar.js
+++ first-app/src/components/Navbar.js
@@ -1,15 +1,6 @@
 import React, {useState } from "react";
-import Logo from "../Assets/Logo.svg";
-import { BsCart2 } from "react-icons/bs";
-import { HiOutlineBars3 } from "react-icons/hi2";
-import { 
-  Box,
-  Drawer,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
+import Logo from "../Assets/Logo.png";
+
 import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
@@ -29,14 +20,13 @@ const Navbar = () => {
         text: "About",
         Icon: <InfoIcon />,
       },
-      {
-        text: "Testimonials",
-        Icon: <CommentRoundedIcon />
-      },
+
+
       {
         text: "Contact",
         Icon: <PhoneRoundedIcon />,
       },
+
       {
         text: "Cart",
         Icon: <ShoppingCartRoundedIcon />,
@@ -44,46 +34,19 @@ const Navbar = () => {
     ];
  
   return(
-     <nav>
+    <nav>
        <div className="nav-logo-container" >
          <img src={Logo} alt="" />
       </div>
       <div className="navbar-links-conatainer" >
-         <a href="">Home</a>
-         <a href="">About</a>
-         <a href="">Testimonials</a>
-         <a href="">Contact</a>
-         <a href="">
-           <BsCart2 className="navbar-cart-ion" />
-         </a>
-         <button className = "primary-button">Bookings Now</button>
-       </div>
-       <div className="navbar-menu-container">
-        <HiOutlineBars3 onClick={ () => setOpenMenu(true)} />
-       </div>
-       <Drawer open = {openMenu} onClose={ () => setOpenMenu(false)}
-       anchor="right">
-        <Box 
-          sx={{width : 250}}>
-          role="presentation"
-          onClick={() => setOpenMenu(false)}
-          onKeyDown={() => setOpenMenu(false)}
-        >
-           <List>
-                {menuOptions.map((item) => (
-                      <ListItem key={item.text} disablePadding >
-                          <ListItemButton>
-                              <ListItemIcon>{item.Icon}</ListItemIcon>
-                              <ListItemText primary = {item.text} />
-                          </ListItemButton>
-                      </ListItem>
-                ) )}
+         <a className="home" href="">Home</a>
+         <a className="about" href="">About</a>
+         <a className="blog" href="">Blogs</a>
+         <a className="contact" href="contact">Contact</a>
 
-             </List>
-
-         </Box>
-       </Drawer>
-     </nav>
+         <button className = "primary-button">Wishlist</button>
+       </div>
+    </nav>
   );
 };
 
